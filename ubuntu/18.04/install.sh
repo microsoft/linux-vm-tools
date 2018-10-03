b2b4d37b612d47d8bad76d68faffed84c64dee25
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-10-03 20:09:52 -0300

    fix code style

--- ubuntu/18.04/install.sh
+++ ubuntu/18.04/install.sh
@@ -11,7 +11,7 @@
 # Update our machine to the latest code if we need to.
 #
 
-if [ $(id -u) -ne 0 ] ; then
+if [ $(id -u) -ne 0 ]; then
     echo 'This script must be run with root privileges' >&2
     exit 1
 fi
@@ -49,7 +49,7 @@ sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
 sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
 
 # Add script to setup the ubuntu session properly
-if [ ! -e /etc/xrdp/startubuntu.sh ] ; then
+if [ ! -e /etc/xrdp/startubuntu.sh ]; then
 cat >> /etc/xrdp/startubuntu.sh << EOF
 #!/bin/sh
 export GNOME_SHELL_SESSION_MODE=ubuntu
@@ -69,19 +69,19 @@ sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc
 sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 
 # Blacklist the vmw module
-if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ] ; then
+if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
 cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
 blacklist vmw_vsock_vmci_transport
 EOF
 fi
 
 #Ensure hv_sock gets loaded
-if [ ! -e /etc/modules-load.d/hv_sock.conf ] ; then
+if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
 echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
 fi
 
 # Configure the policy xrdp session
-cat >/etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
+cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
 [Allow Colord all Users]
 Identity=unix-user:*
 Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
